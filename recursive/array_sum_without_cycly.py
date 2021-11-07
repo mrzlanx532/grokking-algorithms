51445a54f04161484dc98ebd84f6df68b10c21cb
mrzlanx532 <denis_danilov_97@mail.ru>
2021-11-07 19:13:48 +0300

    Added sum without cycle

---
+++ recursive/array_sum_without_cycly.py
@@ -0,0 +1,14 @@
+def sum(list, acc = 0):
+
+	if len(list) == 0:
+		return acc
+
+	acc = acc + list.pop(0)
+
+	if len(list) == 0:
+		return acc
+	else:
+		return sum(list, acc)
+
+
+print sum([1,2, 4, 7])
